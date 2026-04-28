0734c0f0f9788a0b20d83f3f8bf105c37f6bdf2a
Heet <heetchauhan02@gmail.com>
2026-04-28 12:57:50 +0530

    Add file upload functionality using multer in mt1.js
    - Implemented multer for handling file uploads.
    - Configured storage to save uploaded files in the 'LJU' directory with a specific filename format.
    - Created an endpoint '/upload' to accept multiple file uploads (up to 5 files).
    - Responds with the details of the uploaded files.

---
+++ SEM-4/FSD-2/CH-6/mt1.js
@@ -0,0 +1,22 @@
+express = require('express');
+app = express();
+const multer = require('multer');
+app.use(express.static(__dirname));
+var store  =multer.diskStorage({
+    destination:'LJU',
+    filename:function(req,file,cb){
+        cb(null,file.fieldname + "-file.pdf");
+    }
+});
+
+var upload = multer({storage:store});
+// var upload = multer({storage:store,limit:{fileSize:1024}});
+// app.post('/upload',upload.single('mypic'),(req,res)=>{
+app.post('/upload',upload.array('lju',5),(req,res)=>{
+    const file = req.files;
+    for (let i of file) {
+        res.write(JSON.stringify(i) + '\n');
+    }
+    res.send();
+});
+app.listen(4321);
