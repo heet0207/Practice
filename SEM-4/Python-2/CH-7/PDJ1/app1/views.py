20fb1e3956a06fb005439111b0cdb6fd9bba3610
Heet <heetchauhan02@gmail.com>
2026-05-30 09:31:14 +0530

    django projects

--- SEM-4/Python-2/CH-7/PDJ1/app1/views.py
+++ SEM-4/Python-2/CH-7/PDJ1/app1/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render # type: ignore
+from django.http import HttpResponse # type: ignore
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1> This is new Project </h1>')
