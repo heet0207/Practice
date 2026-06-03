95dfb7eb90e66864d8fda8095c2e95fedd79d879
Heet <heetchauhan02@gmail.com>
2026-06-03 18:41:08 +0530

    Add new image files and implement basic React components
    - Added new JPEG images (h5 copy.jpeg, h5.jpeg, h6.jpeg) to the project.
    - Created a new React component (Lmn) with increment and decrement functionality using useState.
    - Set up main entry point (main.jsx) to render the T1Parent component.
    - Implemented a Parent component that provides context values for first and last names.

--- SEM-4/FSD-2/CH-8/Vite/src/App.jsx
+++ SEM-4/FSD-2/CH-8/Vite/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import reactLogo from '../../as/assets/react.svg'
+import viteLogo from '../../as/assets/vite.svg'
+import heroImg from '../../as/assets/hero.png'
+import '../../as/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- SEM-4/FSD-2/CH-8/as/main.jsx
+++ SEM-4/FSD-2/CH-8/as/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import ReactDOM from 'react-dom/client';
-import '../../as/index.css'
+import '../../../as/index.css'
 // import Lmn from './lmn.jsx'
 // import Btn1 from './btn2.jsx'
 // import Bg from './bg.jsx'
@@ -12,7 +12,7 @@ import '../../as/index.css'
 // import Parent from './parent.jsx'
 // import T1 from './T1.jsx'
 // import T1Parent from './T1_Parent.jsx';
-import T1Parent from '../../as/T1_Parent.jsx';
+import T1Parent from '../../../as/T1_Parent.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <T1Parent/>
